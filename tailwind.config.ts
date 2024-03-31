0e42542eec699e7106132481e8cec8d06be8b68f
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-31 17:57:17 -0300

    update config tailwind colors variable shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,19 +14,52 @@ const config = {
       center: true,
       padding: {
         DEFAULT: '1.5rem',
-        sm: '2rem'
+        md: '2rem'
       },
       screens: {
         DEFAULT: '1120px'
       }
     },
-    fontFamily: {
-      sans: ['var(--font-sans)', 'sans-serif'],
-      heading: ['var(--font-heading)', 'serif']
-    },
     extend: {
-      spacing: {
-        18: '72px'
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
       },
       keyframes: {
         'accordion-down': {
