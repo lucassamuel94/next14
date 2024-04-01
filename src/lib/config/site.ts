9f17f9b217b7df374b68c47776b9a16b49182c4e
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-04-01 17:05:04 -0300

    lib: create config site

---
+++ src/lib/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: 'Boilerplate Next.js',
+  description:
+    'Componentes lindamente projetados construídos com Radix UI e Tailwind CSS.',
+  mainNav: [
+    {
+      title: 'Home',
+      href: '/'
+    }
+  ],
+  links: {
+    twitter: 'https://twitter.com/shadcn',
+    github: 'https://github.com/shadcn/ui',
+    docs: 'https://ui.shadcn.com'
+  }
+}
