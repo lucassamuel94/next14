968471bd06a930b2300d7137ad385e4edca3bdd4
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-04-02 08:57:37 -0300

    component: update metada and add component ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,30 @@
 import type { Metadata } from 'next'
+
 import '@/styles/globals.css'
-import { fontHeading, fontSans } from '@/styles/fonts'
+import { ThemeProvider } from '@/components/theme-provider'
+import { siteConfig } from '@/lib/config/site'
 import { cn } from '@/lib/utils'
+import { fontHeading, fontSans } from '@/styles/fonts'
 
 type RootLayoutProps = {
   children: React.ReactNode
 }
 
 export const metadata: Metadata = {
-  title: 'Next 14 BFB',
-  description:
-    'Boilerplate default de projetos em Next14 desenvolvido por Lucas Samuel'
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' }
+  ],
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png'
+  }
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
@@ -22,7 +36,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
           fontHeading.variable,
           'min-h-screen bg-white font-sans text-neutral-700 antialiased'
         )}>
-        {children}
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <div className='relative flex min-h-screen flex-col'>
+            <div className='flex-1'>{children}</div>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
