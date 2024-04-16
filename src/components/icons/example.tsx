38f8928acec7aeb33c3e6aa4caf46c23ecbcf9b9
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-04-16 16:07:34 -0300

    component: create IconExample

---
+++ src/components/icons/example.tsx
@@ -0,0 +1,21 @@
+import { LucideProps } from 'lucide-react'
+
+export function IconExemplo(props: LucideProps) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 39 30' {...props}>
+      <g clipPath='url(#clip0_2513_1474)'>
+        <path
+          fill='currentColor'
+          d='M1.688 8.906a.9.9 0 00-.66.279.901.901 0 00-.278.659.9.9 0 00.278.659.9.9 0 00.66.278h35.624a.9.9 0 00.66-.278.9.9 0 00.278-.66.901.901 0 00-.278-.658.901.901 0 00-.66-.279H1.688zm0 10.313a.9.9 0 00-.66.278.9.9 0 00-.278.66.9.9 0 00.278.658.9.9 0 00.66.279h35.624a.901.901 0 00.66-.279.9.9 0 00.278-.659.9.9 0 00-.278-.659.9.9 0 00-.66-.278H1.688z'></path>
+      </g>
+      <defs>
+        <clipPath id='clip0_2513_1474'>
+          <path
+            fill='currentColor'
+            d='M0 0H37.5V30H0z'
+            transform='matrix(1 0 0 -1 .75 30)'></path>
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
